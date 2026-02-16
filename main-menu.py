4390c3baeebb3784a772ce50512de84429dd7d6b
calistaruiz <c_m_r@CALISTA-LAPTOP>
2026-02-16 11:44:44 -0800

    made the main menu a little bigger to see better

--- main-menu.py
+++ main-menu.py
@@ -4,7 +4,7 @@ import pygame_menu
 from pygame_menu import themes
  
 pygame.init()
-surface = pygame.display.set_mode((600, 400))
+surface = pygame.display.set_mode((1000, 600))
  
 def set_difficulty(value, difficulty):
     print(value)
@@ -18,16 +18,16 @@ def level_menu():
     mainmenu._open(level)
  
  
-mainmenu = pygame_menu.Menu('Welcome', 600, 400, theme=themes.THEME_SOLARIZED)
+mainmenu = pygame_menu.Menu('Welcome', 1000, 600, theme=themes.THEME_SOLARIZED)
 mainmenu.add.text_input('Name: ', default='username')
 mainmenu.add.button('Play', start_the_game)
 mainmenu.add.button('Levels', level_menu)
 mainmenu.add.button('Quit', pygame_menu.events.EXIT)
  
-level = pygame_menu.Menu('Select a Difficulty', 600, 400, theme=themes.THEME_BLUE)
+level = pygame_menu.Menu('Select a Difficulty', 1000, 600, theme=themes.THEME_BLUE)
 level.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
  
-loading = pygame_menu.Menu('Loading the Game...', 600, 400, theme=themes.THEME_DARK)
+loading = pygame_menu.Menu('Loading the Game...', 1000, 600, theme=themes.THEME_DARK)
 loading.add.progress_bar("Progress", progressbar_id = "1", default=0, width = 200, )
  
 arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
